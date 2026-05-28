259344621270b4b06ef9ee142036bfca959a9b2b
saket <saketbhatnagar2@gmail.com>
2026-05-28 12:38:05 +0530

    initial-commit-serverSetup

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "GitHub Profile Analyzer API Running",
+    });
+});
+
+module.exports = app;
